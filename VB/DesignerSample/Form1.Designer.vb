c407e63d55e4bea786fb176a0eea81cee6771887
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-11-29 17:40:47 +0300

    add vb project

---
+++ VB/DesignerSample/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace DesignerSample
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Me.dashboardDesigner1 = New DevExpress.DashboardWin.DashboardDesigner()
+			Me.svgImageCollection1 = New DevExpress.Utils.SvgImageCollection(Me.components)
+			CType(Me.dashboardDesigner1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.svgImageCollection1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' dashboardDesigner1
+			' 
+			Me.dashboardDesigner1.AllowInspectAggregatedData = True
+			Me.dashboardDesigner1.AllowInspectRawData = True
+			Me.dashboardDesigner1.AllowPrintDashboard = False
+			Me.dashboardDesigner1.AllowPrintDashboardItems = False
+			Me.dashboardDesigner1.Appearance.BackColor = System.Drawing.Color.FromArgb((CInt(Fix((CByte(240))))), (CInt(Fix((CByte(240))))), (CInt(Fix((CByte(240))))))
+			Me.dashboardDesigner1.Appearance.Options.UseBackColor = True
+			Me.dashboardDesigner1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.dashboardDesigner1.Location = New System.Drawing.Point(0, 0)
+			Me.dashboardDesigner1.Name = "dashboardDesigner1"
+			Me.dashboardDesigner1.Size = New System.Drawing.Size(1096, 473)
+			Me.dashboardDesigner1.TabIndex = 0
+            '			Me.dashboardDesigner1.DashboardItemClick += New DevExpress.DashboardWin.DashboardItemMouseActionEventHandler(Me.OnDashboardItemClick);
+            '			Me.dashboardDesigner1.DashboardItemControlCreated += New DevExpress.DashboardWin.DashboardItemControlCreatedEventHandler(Me.OnDashboardItemControlCreated);
+            '			Me.dashboardDesigner1.CustomizeDashboardItemCaption += New DevExpress.DashboardWin.CustomizeDashboardItemCaptionEventHandler(Me.OnCustomizeDashboardItemCaption);
+            ' 
+            ' svgImageCollection1
+            ' 
+            Me.svgImageCollection1.Add("ClearMasterFilter", "ClearMasterFilter", GetType(My.Resources))
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(1096, 473)
+			Me.Controls.Add(Me.dashboardDesigner1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(Me.dashboardDesigner1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.svgImageCollection1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private WithEvents dashboardDesigner1 As DevExpress.DashboardWin.DashboardDesigner
+		Private svgImageCollection1 As DevExpress.Utils.SvgImageCollection
+	End Class
+End Namespace
---
+++ VB/DesignerSample/Form1.vb
@@ -0,0 +1,127 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Windows.Forms
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon.ViewerData
+Imports DevExpress.DashboardWin
+Imports DevExpress.XtraPivotGrid
+Imports DevExpress.XtraEditors
+
+Namespace DesignerSample
+	Partial Public Class Form1
+		Inherits XtraForm
+		''' <summary>
+		''' Determines whether filtering should be skipped (the Expand/Collapse button is pressed)
+		''' </summary>
+		Private skipFiltering As Boolean = False
+
+		Public Sub New()
+			InitializeComponent()
+			dashboardDesigner1.CreateRibbon()
+			dashboardDesigner1.LoadDashboard("PivotMasterFilter.xml")
+		End Sub
+
+		''' <summary>
+		''' Used to handle the MouseDown event of the underlying PivotGridControl and determine if the Expand/Collapse button is pressed
+		''' </summary>
+		Private Sub OnDashboardItemControlCreated(ByVal sender As Object, ByVal e As DashboardItemControlEventArgs) Handles dashboardDesigner1.DashboardItemControlCreated
+			If e.DashboardItemName = "pivotDashboardItem1" Then
+				AddHandler e.PivotGridControl.MouseDown, AddressOf OnPivotGridControlMouseDown
+			End If
+		End Sub
+
+		''' <summary>
+		''' Used to skip DashboardItemClick processing if the Expand/Collapse button is pressed
+		''' </summary>
+		Private Sub OnPivotGridControlMouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
+			Dim pivot As PivotGridControl = TryCast(sender, PivotGridControl)
+			Dim hi As PivotGridHitInfo = pivot.CalcHitInfo(e.Location)
+			skipFiltering = (hi.ValueInfo IsNot Nothing AndAlso hi.ValueInfo.ValueHitTest = PivotGridValueHitTest.ExpandButton)
+		End Sub
+
+		''' <summary>
+		''' Used to get dimensions' values related to a clicked area and set parameters used for filtering
+		''' </summary>
+		Private Sub OnDashboardItemClick(ByVal sender As Object, ByVal e As DashboardItemMouseActionEventArgs) Handles dashboardDesigner1.DashboardItemClick
+			If e.DashboardItemName = "pivotDashboardItem1" AndAlso (Not skipFiltering) Then
+				dashboardDesigner1.BeginUpdateParameters()
+				'clear all parameters
+				ClearPivotFilter()
+				'set selected columns and rows to parameters
+				SetParameterValue(e.GetAxisPoint("Column"))
+				SetParameterValue(e.GetAxisPoint("Row"))
+				dashboardDesigner1.EndUpdateParameters()
+			End If
+		End Sub
+
+		''' <summary>
+		''' Sets Parameters' values for dimensions from a specific axis (Column or Row)
+		''' </summary>
+		Private Sub SetParameterValue(ByVal axisPoint As AxisPoint)
+			Do While axisPoint IsNot Nothing AndAlso axisPoint.Dimension IsNot Nothing
+				Dim parameterName As String = GetParameterDataMember(axisPoint.Dimension.DataMember)
+				dashboardDesigner1.Parameters(parameterName).SelectedValue = axisPoint.Value
+				axisPoint = axisPoint.Parent
+			Loop
+		End Sub
+
+		''' <summary>
+		''' Used to add the Clear Filter button to the PivotGrid Item's caption
+		''' </summary>
+		Private Sub OnCustomizeDashboardItemCaption(ByVal sender As Object, ByVal e As CustomizeDashboardItemCaptionEventArgs) Handles dashboardDesigner1.CustomizeDashboardItemCaption
+			If e.DashboardItemName = "pivotDashboardItem1" Then
+				Dim showDataItem As New DashboardToolbarItem("Clear Master Filter", New Action(Of DashboardToolbarItemClickEventArgs)(Function(args) AnonymousMethod1(args)))
+				showDataItem.Enabled = IsAnyFilterSet()
+				showDataItem.SvgImage = svgImageCollection1(0)
+				e.Items.Insert(0, showDataItem)
+			End If
+		End Sub
+		
+		Private Function AnonymousMethod1(ByVal args As Object) As Boolean
+			dashboardDesigner1.BeginUpdateParameters()
+			ClearPivotFilter()
+			dashboardDesigner1.EndUpdateParameters()
+			Return True
+		End Function
+
+		''' <summary>
+		''' Clears all parameters related to the PivotGrid Item
+		''' </summary>
+		Private Sub ClearPivotFilter()
+			Dim pivotItem As PivotDashboardItem = TryCast(dashboardDesigner1.Dashboard.Items("pivotDashboardItem1"), PivotDashboardItem)
+			ClearParameters(pivotItem.Columns)
+			ClearParameters(pivotItem.Rows)
+		End Sub
+
+		''' <summary>
+		''' Clears Prarameters' values for all dimensions form a specific collection
+		''' </summary>
+		Private Sub ClearParameters(ByVal dimensions As DimensionCollection)
+			For Each dimension In dimensions
+				dashboardDesigner1.Parameters(GetParameterDataMember(dimension.DataMember)).SelectedValue = Nothing
+			Next dimension
+		End Sub
+
+		''' <summary>
+		''' Gets a value indicating if any parameter related to the PivotGrid Item has value
+		''' </summary>
+		Private Function IsAnyFilterSet() As Boolean
+			Dim pivotItem As PivotDashboardItem = TryCast(dashboardDesigner1.Dashboard.Items("pivotDashboardItem1"), PivotDashboardItem)
+			If pivotItem.Columns.Any(Function(c) dashboardDesigner1.Parameters(GetParameterDataMember(c.DataMember)).SelectedValue IsNot Nothing) Then
+				Return True
+			End If
+			If pivotItem.Rows.Any(Function(c) dashboardDesigner1.Parameters(GetParameterDataMember(c.DataMember)).SelectedValue IsNot Nothing) Then
+				Return True
+			End If
+			Return False
+		End Function
+
+		''' <summary>
+		''' Generates a parameter's name from a data member
+		''' </summary>
+		Private Function GetParameterDataMember(ByVal dataMember As String) As String
+			Return dataMember & "Param"
+		End Function
+	End Class
+End Namespace
