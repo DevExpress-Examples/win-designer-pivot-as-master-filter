566c1a479aca1cbef2ea00e20fb64e8fbf7dfd0f
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-11-29 16:22:15 +0300

    add CS solution

---
+++ CS/DesignerSample/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace DesignerSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dashboardDesigner1 = new DevExpress.DashboardWin.DashboardDesigner();
+            this.svgImageCollection1 = new DevExpress.Utils.SvgImageCollection(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardDesigner1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dashboardDesigner1
+            // 
+            this.dashboardDesigner1.AllowInspectAggregatedData = true;
+            this.dashboardDesigner1.AllowInspectRawData = true;
+            this.dashboardDesigner1.AllowPrintDashboard = false;
+            this.dashboardDesigner1.AllowPrintDashboardItems = false;
+            this.dashboardDesigner1.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.dashboardDesigner1.Appearance.Options.UseBackColor = true;
+            this.dashboardDesigner1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardDesigner1.Location = new System.Drawing.Point(0, 0);
+            this.dashboardDesigner1.Name = "dashboardDesigner1";
+            this.dashboardDesigner1.Size = new System.Drawing.Size(1096, 473);
+            this.dashboardDesigner1.TabIndex = 0;
+            this.dashboardDesigner1.DashboardItemClick += new DevExpress.DashboardWin.DashboardItemMouseActionEventHandler(this.OnDashboardItemClick);
+            this.dashboardDesigner1.DashboardItemControlCreated += new DevExpress.DashboardWin.DashboardItemControlCreatedEventHandler(this.OnDashboardItemControlCreated);
+            this.dashboardDesigner1.CustomizeDashboardItemCaption += new DevExpress.DashboardWin.CustomizeDashboardItemCaptionEventHandler(this.OnCustomizeDashboardItemCaption);
+            // 
+            // svgImageCollection1
+            // 
+            this.svgImageCollection1.Add("ClearMasterFilter", "ClearMasterFilter", typeof(DesignerSample.Properties.Resources));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 473);
+            this.Controls.Add(this.dashboardDesigner1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardDesigner1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardWin.DashboardDesigner dashboardDesigner1;
+        private DevExpress.Utils.SvgImageCollection svgImageCollection1;
+    }
+}
---
+++ CS/DesignerSample/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.DashboardCommon;
+using DevExpress.DashboardCommon.ViewerData;
+using DevExpress.DashboardWin;
+using DevExpress.XtraPivotGrid;
+using DevExpress.XtraEditors;
+
+namespace DesignerSample {
+    public partial class Form1 : XtraForm {
+        /// <summary>
+        /// Determines whether filtering should be skipped (the Expand/Collapse button is pressed)
+        /// </summary>
+        bool skipFiltering = false;
+
+        public Form1() {
+            InitializeComponent();
+            dashboardDesigner1.CreateRibbon();
+            dashboardDesigner1.LoadDashboard("PivotMasterFilter.xml");
+        }
+
+        /// <summary>
+        /// Used to handle the MouseDown event of the underlying PivotGridControl and determine if the Expand/Collapse button is pressed
+        /// </summary>
+        void OnDashboardItemControlCreated(object sender, DashboardItemControlEventArgs e) {
+            if(e.DashboardItemName == "pivotDashboardItem1") {
+                e.PivotGridControl.MouseDown += OnPivotGridControlMouseDown;
+            }
+        }
+
+        /// <summary>
+        /// Used to skip DashboardItemClick processing if the Expand/Collapse button is pressed
+        /// </summary>
+        void OnPivotGridControlMouseDown(object sender, MouseEventArgs e) {
+            PivotGridControl pivot = sender as PivotGridControl;
+            PivotGridHitInfo hi = pivot.CalcHitInfo(e.Location);
+            skipFiltering = (hi.ValueInfo != null && hi.ValueInfo.ValueHitTest == PivotGridValueHitTest.ExpandButton);
+        }
+
+        /// <summary>
+        /// Used to get dimensions' values related to a clicked area and set parameters used for filtering
+        /// </summary>
+        void OnDashboardItemClick(object sender, DashboardItemMouseActionEventArgs e) {
+            if(e.DashboardItemName == "pivotDashboardItem1" && !skipFiltering) {
+                dashboardDesigner1.BeginUpdateParameters();
+                //clear all parameters
+                ClearPivotFilter();
+                //set selected columns and rows to parameters
+                SetParameterValue(e.GetAxisPoint("Column"));
+                SetParameterValue(e.GetAxisPoint("Row"));
+                dashboardDesigner1.EndUpdateParameters();
+            }
+        }
+
+        /// <summary>
+        /// Sets Parameters' values for dimensions from a specific axis (Column or Row)
+        /// </summary>
+        void SetParameterValue(AxisPoint axisPoint) {
+            while(axisPoint != null && axisPoint.Dimension != null) {
+                string parameterName = GetParameterDataMember(axisPoint.Dimension.DataMember);
+                dashboardDesigner1.Parameters[parameterName].SelectedValue = axisPoint.Value;
+                axisPoint = axisPoint.Parent;
+            }
+        }
+
+        /// <summary>
+        /// Used to add the Clear Filter button to the PivotGrid Item's caption
+        /// </summary>
+        void OnCustomizeDashboardItemCaption(object sender, CustomizeDashboardItemCaptionEventArgs e) {
+            if(e.DashboardItemName == "pivotDashboardItem1") {
+                DashboardToolbarItem showDataItem = new DashboardToolbarItem("Clear Master Filter",
+                        new Action<DashboardToolbarItemClickEventArgs>((args) => {
+                            dashboardDesigner1.BeginUpdateParameters();
+                            ClearPivotFilter();
+                            dashboardDesigner1.EndUpdateParameters();
+                        }));
+                showDataItem.Enabled = IsAnyFilterSet();
+                showDataItem.SvgImage = svgImageCollection1[0];
+                e.Items.Insert(0, showDataItem);
+            }
+        }
+
+        /// <summary>
+        /// Clears all parameters related to the PivotGrid Item
+        /// </summary>
+        void ClearPivotFilter() {
+            PivotDashboardItem pivotItem = dashboardDesigner1.Dashboard.Items["pivotDashboardItem1"] as PivotDashboardItem;
+            ClearParameters(pivotItem.Columns);
+            ClearParameters(pivotItem.Rows);
+        }
+
+        /// <summary>
+        /// Clears Prarameters' values for all dimensions form a specific collection
+        /// </summary>
+        void ClearParameters(DimensionCollection dimensions) {
+            foreach(var dimension in dimensions)
+                dashboardDesigner1.Parameters[GetParameterDataMember(dimension.DataMember)].SelectedValue = null;
+        }
+
+        /// <summary>
+        /// Gets a value indicating if any parameter related to the PivotGrid Item has value
+        /// </summary>
+        bool IsAnyFilterSet() {
+            PivotDashboardItem pivotItem = dashboardDesigner1.Dashboard.Items["pivotDashboardItem1"] as PivotDashboardItem;
+            if(pivotItem.Columns.Any(c => dashboardDesigner1.Parameters[GetParameterDataMember(c.DataMember)].SelectedValue != null))
+                return true;
+            if(pivotItem.Rows.Any(c => dashboardDesigner1.Parameters[GetParameterDataMember(c.DataMember)].SelectedValue != null))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Generates a parameter's name from a data member
+        /// </summary>
+        string GetParameterDataMember(string dataMember) {
+            return dataMember + "Param";
+        }
+    }
+}
